0ab3615306d0726d530fed16c11ce0e092145f12
Pavel Djundik <github@xpaw.me>
2016-12-18 11:24:50 +0200

    Implement away message restoration on reconnections and auto away

--- test/models/network.js
+++ test/models/network.js
@@ -11,6 +11,7 @@ describe("Network", function() {
 
 		it("should produce an valid object", function() {
 			var network = new Network({
+				awayMessage: "I am away",
 				name: "networkName",
 				channels: [
 					new Chan({name: "#thelounge"}),
@@ -22,6 +23,7 @@ describe("Network", function() {
 			network.setNick("chillin`");
 
 			expect(network.export()).to.deep.equal({
+				awayMessage: "I am away",
 				name: "networkName",
 				host: "",
 				port: 6667,
